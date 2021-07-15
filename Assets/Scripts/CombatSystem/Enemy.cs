8badad9ef8078aca0bfc55fc6d723adca883596e
mkovalevwork <mkovalevwork@gmail.com>
2021-07-15 19:50:50 +0300

    CombatSystem Scripts

---
+++ Assets/Scripts/CombatSystem/Enemy.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Animator animator;
+
+    public int maxHealth = 100;
+    int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        //Play hurt anumation
+        animator.SetTrigger("Hurt");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("enemy died");
+        // die animation
+        animator.SetBool("IsDead", true);
+
+        // disable enemy
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<EnemyController>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Assets/Scripts/CombatSystem/PlayerCombat.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask enemyLayers;
+
+    public float attackRange;
+    public int attackDamage = 40;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            Attack();
+        }
+    }
+
+    
+    void Attack()
+    {
+        // play an attack animation
+        animator.SetTrigger("Attack");
+
+
+        // detect enemys in range of attack
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        // Damage them
+        foreach (var enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
