5d0f980c82b9a712434e8be7c1b8f98c4ad84a2e
Alexey <hatesf@mail.ru>
2021-07-25 23:57:07 +0300

    Code update
    Delete unused code from Extinguisher script

--- Assets/Existuisher.cs
+++ Assets/Existuisher.cs
@@ -8,15 +8,6 @@ public class Existuisher : MonoBehaviour
     private bool hasExti = false;
 
     public string extinguisherSound;
-    public string extinguisherEmptySound;
-
-    [FMODUnity.EventRef]
-    private FMOD.Studio.EventInstance eventInstance;
-
-    private void Start()
-    {
-        eventInstance = FMODUnity.RuntimeManager.CreateInstance(extinguisherSound);
-    }
 
     void FixedUpdate()
     {
@@ -28,12 +19,6 @@ public class Existuisher : MonoBehaviour
     {
         if (Input.GetMouseButton(1) && amount > 0 && hasExti)
         {
-            // Set event parameter. Transform to "from 0 to 1" value.
-            eventInstance.setParameterByName("extinguisher_charge", amount / 1000);
-
-            // Play fmod event.
-            eventInstance.start();
-
             Debug.Log("Mouse pressed");
             system.Play(true);            
             amount -= costPerFixedUpdate;
@@ -46,9 +31,6 @@ public class Existuisher : MonoBehaviour
        
         else
         {
-            // Stop fmod event.
-            eventInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
-
             system.Stop(true, ParticleSystemStopBehavior.StopEmitting);
             Debug.Log("Mouse notpressed");           
         }
